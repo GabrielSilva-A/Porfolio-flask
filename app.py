8d4b883366f28c9106b94383e6bdd26c3fda41fd
Gabriel Silva <ags0014@gmail.com>
2025-11-23 23:17:55 -0300

    Actualizacion de badge

--- app.py
+++ app.py
@@ -18,15 +18,15 @@ portfolio_data = {
     'location': 'Rojas, Argentina',
     'about': 'Soy estudiante en Programación, mi mayor fortaleza es mi capacidad de aprendizaje y la adaptabilidad. Me tomo con entusiasmo cada nuevo desafio porque es la oportunidad perfecta para aprender algo nuevo.',
     'skills': [
-        {'name': 'Python', 'level': ' Básico', 'category': 'Backend', 'icon': 'fab fa-python'},
-        {'name': 'Flask', 'level': ' Básico', 'category': 'Backend', 'icon': 'fas fa-flask'},
-        {'name': 'JavaScript', 'level': ' Básico', 'category': 'Frontend', 'icon': 'fab fa-js-square'},
-        {'name': 'HTML/CSS', 'level': ' Básico', 'category': 'Frontend', 'icon': 'fab fa-html5'},
+        {'name': 'Python', 'level': ' Intermedio', 'category': 'Backend', 'icon': 'fab fa-python'},
+        {'name': 'Flask', 'level': ' Intermedio', 'category': 'Backend', 'icon': 'fas fa-flask'},
+        {'name': 'JavaScript', 'level': ' Intermedio', 'category': 'Frontend', 'icon': 'fab fa-js-square'},
+        {'name': 'HTML/CSS', 'level': ' Intermedio', 'category': 'Frontend', 'icon': 'fab fa-html5'},
         {'name': 'Git', 'level': ' Básico', 'category': 'Herramientas', 'icon': 'fab fa-git-alt'},
         {'name': 'SQL', 'level': ' Básico', 'category': 'Base de Datos', 'icon': 'fas fa-database'},
         {'name': 'Bootstrap', 'level': ' Básico', 'category': 'Frontend', 'icon': 'fab fa-bootstrap'},
         {'name': 'Node.js', 'level': ' Básico', 'category': 'Backend', 'icon': 'fab fa-node-js'},
-        {'name': 'Express', 'level': ' Básico', 'category': 'Backend', 'icon': 'fas fa-server'}
+        {'name': 'Express', 'level': ' Avanzado', 'category': 'Backend', 'icon': 'fas fa-server'}
     ],
     'projects': [
         {
